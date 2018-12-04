9687121b759257fafbd66582b9cc4a5b1f46018e
kduong@stuy.edu <kduong@stuy.edu>
2018-12-04 18:12:21 -0500

    added comments

--- Sorts.java
+++ Sorts.java
@@ -2,15 +2,15 @@ public class Sorts{
 
   public static int findSmall(int[] ary, int start) {
     int x = 0;
-    for (int i = start; i < ary.length - 1; i ++) {
-      x = Math.min(ary[i], ary[i + 1]);
+    for (int i = start; i < ary.length - 1; i ++) { //keeps repeating until find min of all numbers
+      x = Math.min(ary[i], ary[i + 1]); //find min between two numbers
     }
     return x;
   }
 
   public static void selectionsort(int [] ary) {
-    for (int i = 0; i > ary.length; i --) {
-      ary[i] = findSmall(ary, i);
+    for (int i = 0; i < ary.length; i ++) {  //does it until you finish the going through the array
+      ary[i] = findSmall(ary, i); //set i element equal to the smallest
     }
   }
 
