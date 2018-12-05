c1c93bcd62c1956248d47370e321d2fc9e12e6e9
kduong@stuy.edu <kduong@stuy.edu>
2018-12-05 10:30:00 -0500

    tested with examples on website

--- Sorts.java
+++ Sorts.java
@@ -1,6 +1,6 @@
 public class Sorts{
 
-  public static void selectionsort(int [] ary) {
+  public static void selectionSort(int [] ary) {
     for (int i = 0; i < ary.length - 1; i ++) {
       int min = ary[i];
       int index = i;
@@ -12,6 +12,7 @@ public class Sorts{
       }
       ary[index] = ary[i]; //set element at the index of the min to the element of the index you are replacing
       ary[i] = min; //set element to the minimum of the ary to the right of the specfied index
+      System.out.println(printArray(ary));
     }
   }
 
@@ -23,17 +24,29 @@ public class Sorts{
     return output += ary[ary.length-1] + "]";
 }
 
-/*public static void main(String[] args) {
+public static void main(String[] args) {
 
     // Google random int generator
 
     String sorted = "";
+    int[] test_a = {5, 1, 8, 4, 9, 6, 7, 2, 3};
+    System.out.println(printArray(test_a));
+    selectionSort(test_a);
+    sorted = printArray(test_a);
+    System.out.println(sorted);
+
+    int[] test_b = {5, 1, 12, -5, 16, 2, 12, 14};
+    System.out.println(printArray(test_b));
+    selectionSort(test_b);
+    sorted = printArray(test_b);
+    System.out.println(sorted);
 
     // 8 nums, [0:10]
-    int[] test_a = { 3, 4, 7, 1, 6, 2, 8, 6 };
+    /*int[] test_a = { 3, 4, 7, 1, 6, 2, 8, 6 };
     System.out.println(printArray(test_a));               // [3, 4, 7, 1, 6, 2, 8, 6]
 
-    selectionsort(test_a);
+
+
 
     sorted = printArray(test_a);
     System.out.println(sorted);               // [1, 2, 3, 4, 6, 6, 7, 8]
@@ -48,7 +61,7 @@ public class Sorts{
     int[] test_b = { 17, 1, 19, 2, 18, 20, 1, 18, 11, 13, 5, 17 };
     System.out.println(printArray(test_b));   // [17, 1, 19, 2, 18, 20, 1, 18, 11, 13, 5, 17]
 
-    selectionsort(test_b);
+    selectionSort(test_b);
 
     sorted = printArray(test_b);
     System.out.println(sorted);               // [1, 1, 2, 5, 11, 13, 17, 17, 18, 18, 19, 20]
@@ -63,7 +76,7 @@ public class Sorts{
     int[] test_c = { 14, 32, 19, 66, 61, 96, 33, 48, 30, 100, 65, 37, 15, 85, 1, 35, 9, 57, 50, 52 };
     System.out.println(printArray(test_c));   // [14, 32, 19, 66, 61, 96, 33, 48, 30, 100, 65, 37, 15, 85, 1, 35, 9, 57, 50, 52]
 
-    selectionsort(test_c);
+    selectionSort(test_c);
 
     sorted = printArray(test_c);
     System.out.println(sorted);               // [1, 9, 14, 15, 19, 30, 32, 33, 35, 37, 48, 50, 52, 57, 61, 65, 66, 85, 96, 100]
